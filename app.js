7c47273ad09baf6558182c0896504dff36bbd15e
moizawan07 <moizahmedawan321@gmail.com>
2024-11-09 13:07:51 -0800

    Dom-Child-Nodes

---
+++ app.js
@@ -0,0 +1,37 @@
+function navSection(){
+    let elemnet = document.childNodes[1].childNodes[2].childNodes[1].childNodes[0].nodeValue;
+    alert(elemnet)
+}
+
+function navFirst(){
+    let elemnet = document.childNodes[1].childNodes[2].childNodes[3].childNodes[1].childNodes[1].childNodes[0].nodeValue;
+ alert(elemnet)
+}
+
+function navSecond(){
+    let elemnet = document.childNodes[1].childNodes[2].childNodes[3].childNodes[3].childNodes[1].childNodes[3].childNodes[0].nodeValue;
+    alert(elemnet)
+}
+
+function navThird(){
+    let elemnet = document.childNodes[1].childNodes[2].childNodes[3].childNodes[3].childNodes[3].childNodes[3].childNodes[0].nodeValue;
+    alert(elemnet)
+}
+
+function mainSection(){
+    let elemnet = document.childNodes[1].childNodes[2].childNodes[5].childNodes[0].nodeValue;
+    alert(elemnet)
+}
+function mainfirst(){
+    let elemnet = document.childNodes[1].childNodes[2].childNodes[7].childNodes[1].childNodes[1].childNodes[0].nodeValue;
+    alert(elemnet)
+}
+function mainSecond(){
+    let elemnet = document.childNodes[1].childNodes[2].childNodes[7].childNodes[3].childNodes[1].childNodes[0].nodeValue;
+    alert(elemnet)
+}
+function mainThird(){
+    let elemnet = document.childNodes[1].childNodes[2].childNodes[7].childNodes[7].childNodes[0].nodeValue;
+    alert(elemnet)
+}
+
